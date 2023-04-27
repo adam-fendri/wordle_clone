cdc44d4cbe38bae789cec92ecccf999dfac0a80b
bitrex <adamfendr@insat.u-carthage.tn>
2023-04-27 21:47:29 +0100

    wordle

---
+++ src/Words.js
@@ -0,0 +1,23 @@
+import wordBank from "./wordle-bank.txt";
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordBank)
+    .then((res) => res.text())
+    .then((res) => {
+      const wordArr = res.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      wordSet = new Set(wordArr);
+    });
+
+  return { wordSet, todaysWord };
+};
